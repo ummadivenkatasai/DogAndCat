9078f0ae3bc3577593a6acc7dd20d18f12d985bc
ummadivenkatasai <uvenkatasai99@gmail.com>
2025-04-30 18:11:40 +0530

    navigation component completed

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import '../componentsCss/navigation.css'
 import { Link } from 'react-router-dom'
 import { CIcon } from '@coreui/icons-react';
-import { cilDog, cilCat } from '@coreui/icons'
+import { cilDog, cilCat, cilUser } from '@coreui/icons'
 
 function Navigation() {
   return (
     <div className='navigation' >
       <div className="websiteName">
-        <Link to='/' >DOG&CAT</Link>
+        <Link to='/' >Petpals</Link>
       </div>
       <div className="iconsContent">
-      <div className="dogIcon icon">
+      <div className="dogIcon icon links">
         <Link to="dog">{<CIcon icon={cilDog} />}</Link>
       </div>
-      <div className="catIcon icon">
+      <div className="catIcon icon links">
         <Link to="cat">{< CIcon icon={cilCat} />}</Link>
       </div>
-      <div className="orders">
+      <div className="orders btn links">
         <Link to="orders">orders</Link>
       </div>
-      <div className="singin">
-        <Link to="login">SingIn</Link>
+      <div className="singin btn links">
+        <Link to="login">{< CIcon icon={cilUser} />}singin</Link>
       </div>
       </div>
     </div>
