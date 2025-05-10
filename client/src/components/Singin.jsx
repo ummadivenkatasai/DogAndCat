e4cf9a25593f334f53a44462e6646bcd0ae4eb1f
ummadivenkatasai <uvenkatasai99@gmail.com>
2025-05-10 16:43:46 +0530

    add error message to email and password input

--- client/src/components/Singin.jsx
+++ client/src/components/Singin.jsx
@@ -37,14 +37,15 @@ function Signin() {
     <Grid container className='signinForm' >
       <form className='signinContent' onSubmit={sendData} >
         <Typography variant='h5' >Singin</Typography>
-        {error && <p style={{ color: "red" }}>{error}</p>}
+        {error && <span style={{ color: "red" }}>{error}</span>}
         <Grid className='inputData email' size={8} >
           <TextField variant='outlined' name='email' value={formData.email} onChange={handleChange} placeholder='Email' sx={{minWidth:262,maxWidth:400}}  />
+          {formData.email === '' && fieldError ? <span style={{ color: 'red' }} >{fieldError}</span> : '' }
         </Grid>
         <Grid className='inputData password' size={8} >
           <TextField variant='outlined' name='password' value={formData.password} onChange={handleChange} type='password' placeholder='Password' sx={{minWidth:262,maxWidth:400}}  />
+          {formData.password === '' && fieldError ? <span style={{ color: 'red' }} >{fieldError}</span> : '' }
         </Grid>
-        {fieldError && <p style={{ color: 'red' }}>{fieldError}</p>}
         <Grid className='inputData loginBtn' >
           <Button variant='contained' sx={{minWidth:200}} type='submit' >Login</Button>
         </Grid>
