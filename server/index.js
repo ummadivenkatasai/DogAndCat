304387ca23af3dff773222e038dff413289c08aa
ummadivenkatasai <uvenkatasai99@gmail.com>
2025-07-24 15:55:08 +0530

    some changes in index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const createServer = require('./server');
 
 const app = createServer();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`server is running on ${port}`)
 })
